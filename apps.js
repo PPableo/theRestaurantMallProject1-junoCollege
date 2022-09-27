ae2b27c71cfd2a4ca84ab8a3161ddbba365cc747
PPableo <pableop@gmail.com>
2022-09-27 17:30:08 -0400

    created rating Page, styling, and js - 1st iteration/modified

---
+++ apps.js
@@ -0,0 +1,32 @@
+// active state
+const ratings = document.querySelectorAll(".ratingCircle");
+let selectedRating;
+ratings.forEach((item, i) => {
+  item.addEventListener("click", (event) => {
+    //handle click
+    ratings.forEach((item) => {
+      item.classList.remove("orange-background");
+    });
+
+    item.classList.add("orange-background");
+    console.log(item.innerText);
+    selectedRating = item.innerText;
+  });
+});
+
+// hides the initial view ratings on submit
+const submitButton = document.querySelector(".submit");
+const initialView = document.querySelector(".initialView");
+const submittedView = document.querySelector(".submittedView");
+const spanRating = document.querySelector(".rating");
+
+// will show the submitted rating and thank you message
+submitButton.addEventListener("click", (event) => {
+  ratings.forEach((rating) => {
+    if (rating.classList.contains("orange-background")) {
+      initialView.classList.add("hidden");
+      submittedView.classList.remove("hidden");
+      spanRating.innerText = selectedRating;
+    }
+  });
+});
