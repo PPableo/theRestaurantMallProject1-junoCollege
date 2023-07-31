f2611a07dc6c2bd9ff62ea370f5488d217f55e62
PPableo <pableop@gmail.com>
2023-07-31 13:41:41 -0400

    revamped and updated 1 year later

---
+++ script.js
@@ -0,0 +1,16 @@
+const element = document.querySelector(".menuBar");
+const menuBarElement = document.querySelector(".menuBarButton");
+const closeElement = document.querySelector(".closeButton");
+
+menuBarElement.addEventListener("click", function(){
+    element.style.visibility = "visible";
+    element.style.opacity = "1";
+    menuBarElement.style.visibility = "hidden";
+})
+
+closeElement.addEventListener("click", function(){
+    menuBarElement.style.visibility = "visible";
+    element.style.visibility = "hidden";
+    element.style.opacity = "0";
+    element.style.transition = "visibility 0.5s, opacity 0.5s linear";
+})
